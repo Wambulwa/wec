83e0ad66aef8fe35a27d929d3b06910ab868e91e
Wambulwa <wambulwaayub@gmail.com>
2018-02-05 13:38:07 +0300

    Add files via upload

---
+++ application/views/sea/footer.php
@@ -0,0 +1,27 @@
+  <!-- build:js({.tmp,app}) scripts/app.min.js -->
+  <script src="<?php echo base_url();?>scripts/helpers/modernizr.js"></script>
+  <script src="<?php echo base_url();?>vendor/jquery/dist/jquery.js"></script>
+  <script src="<?php echo base_url();?>vendor/bootstrap/dist/js/bootstrap.js"></script>
+  <script src="<?php echo base_url();?>vendor/fastclick/lib/fastclick.js"></script>
+  <script src="<?php echo base_url();?>vendor/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+  <script src="<?php echo base_url();?>scripts/helpers/smartresize.js"></script>
+  <script src="<?php echo base_url();?>scripts/constants.js"></script>
+  <script src="<?php echo base_url();?>scripts/main.js"></script>
+  <!-- endbuild -->
+  <!-- page scripts -->
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.resize.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.categories.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.stack.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.time.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot/jquery.flot.pie.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot-spline/js/jquery.flot.spline.js"></script>
+  <script src="<?php echo base_url();?>vendor/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+  <!-- end page scripts -->
+  <!-- initialize page scripts -->
+  <script src="<?php echo base_url();?>scripts/helpers/sameheight.js"></script>
+  <script src="<?php echo base_url();?>scripts/ui/dashboard.js"></script>
+  <!-- end initialize page scripts -->
+</body>
+
+</html>
