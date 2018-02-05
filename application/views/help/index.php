7b1b1eee1b6e06851108881ebe093faf15a5f0f1
Wambulwa <wambulwaayub@gmail.com>
2018-02-05 13:28:28 +0300

    Add files via upload

---
+++ application/views/help/index.php
@@ -0,0 +1,193 @@
+      <!-- /top header -->
+      <!-- main area -->
+      <div class="main-content">
+        <div class="m-x-n-g m-t-n-g overflow-hidden">
+          <div class="card m-b-0 bg-primary-dark text-white p-a-md no-border">
+            <h4 class="m-t-0">
+              <span class="pull-right">$ 82,560.00 This week</span>
+              <span>Activity</span>
+              </h4>
+            <div class="chart dashboard-line labels-white" style="height:300px"></div>
+          </div>
+          <div class="card bg-white no-border">
+            <div class="row text-center">
+              <div class="col-sm-3 col-xs-6 p-t p-b">
+                <h4 class="m-t-0 m-b-0">$ 89.34</h4>
+                <small class="text-muted bold">Daily Sales</small>
+              </div>
+              <div class="col-sm-3 col-xs-6 p-t p-b">
+                <h4 class="m-t-0 m-b-0">$ 498.00</h4>
+                <small class="text-muted bold">Weekly Sales</small>
+              </div>
+              <div class="col-sm-3 col-xs-6 p-t p-b">
+                <h4 class="m-t-0 m-b-0">$ 34,903</h4>
+                <small class="text-muted bold">Monthly Sales</small>
+              </div>
+              <div class="col-sm-3 col-xs-6 p-t p-b">
+                <h4 class="m-t-0 m-b-0">$ 98,343.49</h4>
+                <small class="text-muted bold">Yearly Sales</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row same-height-cards">
+          <div class="col-md-8">
+            <div class="card no-border bg-white">
+              <div class="card-block row-equal align-middle">
+                <div class="column p-r">
+                  <div class="h6 text-uppercase">Upcoming due invoices.</div>
+                  <p>All upcoming due invoices will appear here (Those invoices with less than 7 days remaining to be overdue).</p>
+                  <div class="widget-card-title">
+                    <div class="pull-right">
+                      <span class="m-r"><i class="icon-bar-chart text-primary"></i>&nbsp;One-off</span>
+                      <span><i class="icon-bar-chart text-success"></i>&nbsp;Regular</span>
+                    </div>
+                    <h5 class="text-success">$ 56,873 </h5><p>(sum of upcoming due invoices)</p>
+                  </div>
+                </div>
+                <div class="column p-l">
+                  <div class="dashboard-barO" style="height:200px"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card no-border bg-white">
+              <div class="card-block">
+                <div class="text-center p-a">
+                  <h4 class="card-title p-a-lg m-b-0">General updates and news from team for team.</h4>
+                </div>
+                <div class="">
+                  <a href="#" class="card-link pull-right"><i class="icon-clock"></i>&nbsp;Date/Time posted</a>
+                  <a href="#" class="card-link m-l-0">Phoebe Sample</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <div class="card card-block no-border bg-primary">
+              <div class="card-title text-center">
+                <h5 class="m-a-0 text-uppercase">Monthly invoice projection</h5>
+                <!-- <small>the quick brew</small> -->
+              </div>
+              <div class="canvas-holder">
+                <div class="chart labels-white dashboard-bar2" style="height:71px"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-white row-equal align-middle">
+                  <div class="column">
+                    <h6 class="m-a-0 text-uppercase">PAID</h6>
+                    <small class="bold text-muted">$10,499.71</small>
+                  </div>
+                  <div class="column">
+                    <h3 class="m-a-0 text-danger">(40%)</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-white row-equal align-middle">
+                  <div class="column">
+                    <h6 class="m-a-0 text-uppercase">UNPAID</h6>
+                    <small class="bold text-muted">$114.72</small>
+                  </div>
+                  <div class="column">
+                    <h3 class="m-a-0 text-success">(20%)</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-white row-equal align-middle">
+                  <div class="column">
+                    <h6 class="m-a-0 text-uppercase">OVERDUE</h6>
+                    <small class="bold text-muted">$17,392.22</small>
+                  </div>
+                  <div class="column">
+                    <h3 class="m-a-0 text-danger">20%</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-white row-equal align-middle">
+                  <div class="column">
+                    <h6 class="m-a-0 text-uppercase">PARTIALLY PAID</h6>
+                    <small class="bold text-muted">$651.23</small>
+                  </div>
+                  <div class="column">
+                    <h3 class="m-a-0 text-success">(40%)</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-4">
+            <div class="card card-block no-border bg-white">
+              <div class="circle-icon bg-info text-white m-r">
+                <i class="icon-bulb"></i>
+              </div>
+              <div class="overflow-hidden" style="margin-top:1px;">
+                <h4 class="m-a-0">93</h4>
+                <h6 class="m-a-0 text-muted">Invoices created</h6>
+              </div>
+            </div>
+            <div class="card card-block no-border bg-white">
+              <div class="circle-icon bg-danger text-white m-r">
+                <i class="icon-user"></i>
+              </div>
+              <div class="overflow-hidden" style="margin-top:1px;">
+                <h4 class="m-a-0">8.3K (10%)</h4>
+                <h6 class="m-a-0 text-muted">Revenue increase</h6>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-12 col-lg-4">
+            <div class="card card-block no-border bg-white">
+              <div class="circle-icon bg-info text-white m-r">
+                <i class="icon-bulb"></i>
+              </div>
+              <div class="overflow-hidden" style="margin-top:1px;">
+                <h4 class="m-a-0">$760</h4>
+                <h6 class="m-a-0 text-muted">Paid to creditors</h6>
+              </div>
+            </div>
+            <div class="card card-block no-border bg-white">
+              <div class="circle-icon bg-danger text-white m-r">
+                <i class="icon-user"></i>
+              </div>
+              <div class="overflow-hidden" style="margin-top:1px;">
+                <h4 class="m-a-0">$212</h4>
+                <h6 class="m-a-0 text-muted">Still to be paid</h6>
+              </div>
+            </div>
+          </div>
+
+        </div>
+<!--         <div class="row">
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-primary text-white">
+                  <h6 class="m-a-0">NEW USERS</h6>
+                  <h1 class="m-a-0">785</h1>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card card-block no-border bg-dark text-white">
+                  <h6 class="m-a-0">PAGE VIEWS</h6>
+                  <h1 class="m-a-0">381</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> -->
+      </div>
+      <!-- /main area -->
+    </div>
+    <!-- /content panel -->
+    <!-- bottom footer -->
---
+++ application/views/help/left-nav.php
@@ -0,0 +1,243 @@
+<body class="page-loading">
+  <!-- page loading spinner -->
+  <div class="pageload">
+    <div class="pageload-inner">
+      <div class="sk-rotating-plane"></div>
+    </div>
+  </div>
+  <!-- /page loading spinner -->
+  <div class="app layout-fixed-header">
+    <!-- sidebar panel -->
+    <div class="sidebar-panel offscreen-left">
+      <div class="brand">
+        <!-- toggle small sidebar menu -->
+        <a href="javascript:;" class="toggle-apps hidden-xs" data-toggle="quick-launch">
+          <i class="icon-grid"></i>
+        </a>
+        <!-- /toggle small sidebar menu -->
+        <!-- toggle offscreen menu -->
+        <div class="toggle-offscreen">
+          <a href="javascript:;" class="visible-xs hamburger-icon" data-toggle="offscreen" data-move="ltr">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+        <!-- /toggle offscreen menu -->
+        <!-- logo -->
+        <a class="brand-logo">
+          <!-- <span>WILLFREIGHT</span> -->
+        </a>
+        <a href="#" class="small-menu-visible brand-logo">Willfreight</a>
+        <!-- /logo -->
+      </div>
+      <ul class="quick-launch-apps hide">
+        <li>
+          <a href="apps-gallery.html">
+            <span class="app-icon bg-danger text-white">
+            N
+            </span>
+            <span class="app-title">NOTES</span>
+          </a>
+        </li>
+        <li>
+          <a href="apps-messages.html">
+            <span class="app-icon bg-success text-white">
+            N
+            </span>
+            <span class="app-title">NOTIFICATIONS</span>
+          </a>
+        </li>
+        <li>
+          <a href="apps-social.html">
+            <span class="app-icon bg-primary text-white">
+            C
+            </span>
+            <span class="app-title">CLIENTS</span>
+          </a>
+        </li>
+        <li>
+          <a href="apps-travel.html">
+            <span class="app-icon bg-info text-white">
+            R
+            </span>
+            <span class="app-title">REPORTS</span>
+          </a>
+        </li>
+      </ul>
+      <!-- main navigation -->
+      <nav role="navigation">
+        <ul class="nav">
+          <!-- dashboard -->
+          <li>
+            <a href="<?php echo base_url('accounts');?>">
+              <i class="icon-compass"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <!-- /dashboard -->
+          <!-- clients -->
+          <li>
+            <a href="javascript:;">
+              <i class="icon-cursor"></i>
+              <span>MY ACCOUNT</span>
+            </a>
+            <ul class="sub-menu">
+              <li>
+                <a href="apps-calendar.html">
+                  <span>LOGIN</span>
+                </a>
+              </li>
+              <li>
+                <a href="apps-contacts.html">
+                  <span>REGISTER</span>
+                </a>
+              </li>
+              <li>
+                <a href="apps-contacts.html">
+                  <span>MANAGE ACCOUNT</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- /clients -->
+          <!--services-->
+                    <li>
+            <a href="javascript:;">
+              <i class="icon-cursor"></i>
+              <span>ACCOUNTS</span>
+            </a>
+            <ul class="sub-menu">
+              <li>
+                <a href="apps-calendar.html">
+                  <span>CREATE INVOICE</span>
+                </a>
+              </li>
+              <li>
+                <a href="apps-contacts.html">
+                  <span>ADD CLIENT</span>
+                </a>
+              </li>
+              <li>
+                <a href="apps-contacts.html">
+                  <span>GENERATING REPORTS</span>
+                </a>
+              </li>
+<!--               <li>
+                <a href="apps-contacts.html">
+                  <span>GENERATING REPORTS</span>
+                </a>
+              </li>
+ -->            </ul>
+          </li>
+          <!--/services-->
+          <!-- Invoices -->
+          <li>
+            <a href="javascript:;">
+              <i class="icon-loop"></i>
+              <span>SALES</span>
+            </a>
+            <ul class="sub-menu">
+              <li>
+                <a href="forms-basic.html">
+                  <span>ADDING A CLIENT</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>MANAGING LEADS/PROSPECTS</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>CREATING A QUOTE</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!--/invoices-->
+          <!--agents-->
+          <li>
+            <a href="javascript:;">
+              <i class="icon-loop"></i>
+              <span>CUSTOMER SUPPORT</span>
+            </a>
+            <ul class="sub-menu">
+              <li>
+                <a href="forms-basic.html">
+                  <span>ADDING A CUSTOMER</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>ADDING A QUOTE</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>MANAGING TARIFFS</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>MANAGING AIRWAY BILLS</span>
+                </a>
+              </li>
+              <li>
+                <a href="forms-custom.html">
+                  <span>ADDING A CONSIGNMENT</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- /agents -->
+          <!-- reports -->
+         <!--  <li>
+            <a href="javascript:;">
+              <i class="icon-pie-chart"></i>
+              <span>Reports</span>
+            </a>
+            <ul class="sub-menu">
+              <li>
+                <a href="charts-flot.html">
+                  <span>Overview</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-easypie.html">
+                  <span>Agents</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-chartjs.html">
+                  <span>Invoices</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-rickshaw.html">
+                  <span>Customer statements</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-nvd3.html">
+                  <span>Management accounts</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-c3js.html">
+                  <span>Balance sheet</span>
+                </a>
+              </li>
+              <li>
+                <a href="charts-c3js.html">
+                  <span>P&L</span>
+                </a>
+              </li>            
+            </ul>
+          </li> -->
+          <!-- /repoprts -->
+        </ul>
+      </nav>
+      <!-- /main navigation -->
+    </div>
+    <!-- /sidebar panel -->
