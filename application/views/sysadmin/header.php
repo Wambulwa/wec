9e77c32fff8eec2861f6ed4efb58aef54786a4ee
Wambulwa <wambulwaayub@gmail.com>
2018-02-05 13:38:12 +0300

    Add files via upload

---
+++ application/views/sysadmin/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Reactor - Bootstrap Admin Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+  <!-- page stylesheets -->
+  <!-- end page stylesheets -->
+  <!-- build:css({.tmp,app}) styles/app.min.css -->
+<!--   <link rel="stylesheet" href="<?php echo base_url();?>styles/webfont.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/climacons-font.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>vendor/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/font-awesome.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/card.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/sli.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/animate.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/app.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/app.skins.css"> -->
+  <link rel="stylesheet" href="<?php echo base_url()?>styles/webfont.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/climacons-font.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>vendor/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/font-awesome.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/card.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/sli.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/animate.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/app.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>styles/app.skins.css">
+  <!-- endbuild -->
+</head>
